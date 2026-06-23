02aa98d8eab66eaa0cbce70fb9f3187cc8d0e0d9
Danareynolds-coding <dana.reynolds@mscoding.org>
2026-06-23 00:21:12 -0400

    set up dependancies

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
-const express = require('express'); //install express
-const server = express();           //instance of express
-
+const express = require('express') //install express
+const server = express()   //instance of express
+const router = require('./app/routes/router') //install router   
+const helmet = require('helmet') //install helmet
+const cors = require('cors')  //istall cors
 const PORT = process.env.PORT ||3000 //set up port
 
- 
+//set up security so api can API can get through
+server.use(helmet.contentSecurityPolicy({
+  useDefaults:true,
+  crossOriginResourcePolicy:false,
+  crossOriginEmbedderPolicy:false,
+  directives:{
+    "img-src":["'self'", "https: data"],
+    "scriptsSrc":["'self'", "cdn.jsdelivr.net"]
+  }
+})) 
+//set up cors and express
+server.use(cors())
+server.use(express.json())
+server.use(express.urlencoded({extended:true}))
+//setup router localhost:3000=> router.js
+server.use('/',router)
+server.set('view engine','ejs')  //set up view engine
  //http:localhost3000/breeds key= live_WJtegMA39GHMi9CKUyjlG4DBo9LnhF2dVsQtzUotpuw9yzAUjqVF10Q6scTXpG3F'
 
 // root path get request and response -test 3000 works
