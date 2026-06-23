af910c4f6d28ab066fa14853ebd7603bca8140aa
Danareynolds-coding <dana.reynolds@mscoding.org>
2026-06-22 23:03:12 -0400

    add ejs

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express'); //install express
 const server = express();           //instance of express
 
 const PORT = process.env.PORT ||3000 //set up port
-// root path get request and response
-// server.get('/', (req, res)=>{
 
+ 
+ //http:localhost3000/breeds key= live_WJtegMA39GHMi9CKUyjlG4DBo9LnhF2dVsQtzUotpuw9yzAUjqVF10Q6scTXpG3F'
 
-// })
+// root path get request and response -test 3000 works
+//  server.get('/', (req, res)=>{
+//   res.end('My App')
+//  })
+
 server.listen(PORT,()=> console.log(`Server running at local host ${PORT} ! `))   //check port
